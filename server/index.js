25edeb3927a8285071d6214a8bdac6b035ba5206
ramziChammem <ramzichammem69@gmail.com>
2021-02-28 14:23:50 +0100

    redux-store and star rating style

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 require("dotenv").config();
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
 var axios = require("axios");
 
@@ -12,7 +12,6 @@ var MY_KEY = process.env.API_KEY;
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "../public")));
 app.get("/product", (req, res) => {
-  console.log(MY_KEY);
   axios
     .get("https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products", {
       headers: {
